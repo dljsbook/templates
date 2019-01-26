ad351b490315a273ab17ff8ce572ddf506858aa7
Kevin Scott <kevin@hitheory.com>
2019-01-26 15:56:11 -0500

    Add parcel files, initial code, and prettier file

--- inference/index.js
+++ inference/index.js
@@ -1,1 +1,6 @@
-console.log('initial commit')
+import * as tfjs from "@tensorflow/tfjs"
+import { ImageNet } from "@dljsbook/data"
+
+const imageNet = new ImageNet()
+
+imageNet.getImage().then(data => data.print())
