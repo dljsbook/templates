5f1d4903ddfd773acae1f4dff74b58a05f76dcc8
Kevin Scott <kevin@hitheory.com>
2019-01-30 09:54:26 -0500

    Remove all semicolons

--- src/tensors/index.js
+++ src/tensors/index.js
@@ -1,7 +1,7 @@
-import * as tfjs from "@tensorflow/tfjs";
+import * as tfjs from "@tensorflow/tfjs"
 import * as tfvis from "@tensorflow/tfjs-vis"
-import { ImageNet } from "@dljsbook/data";
+import { ImageNet } from "@dljsbook/data"
 
-const imageNet = new ImageNet();
+const imageNet = new ImageNet()
 
-imageNet.getImage().then(data => data.print());
+imageNet.getImage().then(data => data.print())
