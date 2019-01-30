d3044042c0ffa935705d5a47b2fc4786b69eabf0
Kevin Scott <kevin@hitheory.com>
2019-01-30 09:24:39 -0500

    Update build script

--- build.js
+++ build.js
@@ -29,62 +29,79 @@ const getJSFiles = src => {
   return jsFiles;
 };
 
-fs.readdir(src, async (err, dirs) => {
-  try {
-    mkdirp('./dist');
-    for (let i = 0; i < dirs.length; i++) {
-      const dir = dirs[i];
-      const src = `./src/${dir}`;
-      const target = `./dist/${dir}`;
-
-      // 1. create directory
-      await rimraf(target);
-      mkdirp(target);
-
-      // 2. copy prettier file
-      fs.createReadStream('./assets/.prettierrc').pipe(fs.createWriteStream(`${target}/.prettierrc`));
-
-      // 3. copy JSON file and manipulate it
-      const defaultJSON = JSON.parse(fs.readFileSync('./assets/package.json'));
-      const dirJSON = JSON.parse(fs.readFileSync(`${src}/package.json`));
-      const rootJSON = JSON.parse(fs.readFileSync(`./package.json`));
-      const targetJSON = {
-        ...defaultJSON,
-        ...dirJSON,
-        version: rootJSON.version,
-      };
-      fs.writeFileSync(`${target}/package.json`, JSON.stringify(targetJSON, null, 2), 'utf8');
-
-      // 4. copy HTML file
-      const html = fs.readFileSync('./assets/index.html', 'utf8');
-      const $ = cheerio.load(html, {
-        withDomLvl1: true,
-        normalizeWhitespace: false,
-        xmlMode: true,
-      });
-      $('title').text(targetJSON.description);
-      const text = $.html().replace('<script src="./index.js"> </script>', '<script src="./index.js"></script>');
-      fs.writeFileSync(`${target}/index.html`, text, 'utf8');
-
-      // 5. copy js file
-      const jsFiles = getJSFiles(src);
-      jsFiles.forEach(file => {
-        fs.createReadStream(`${src}/${file}`).pipe(fs.createWriteStream(`${target}/${file}`));
-      });
-
-      ghpages.publish('dist', {
-        branch: 'dist',
-      });
-
-      // 6. copy sandbox config
-      copyFile('./assets/sandbox.config.json', `${target}/sandbox.config.json`);
-    }
-  } catch(err) {
-    console.error(err);
-  }
-});
-
 const copyFile = (source, target) => {
   const config = fs.readFileSync(source, 'utf8');
   fs.writeFileSync(target, config, 'utf8');
 }
+
+const buildDir = async (dir) => {
+  const src = `./src/${dir}`;
+  const target = `./dist/${dir}`;
+
+  // 1. create directory
+  await rimraf(target);
+  mkdirp(target);
+
+  // 2. copy prettier file
+  fs.createReadStream('./assets/.prettierrc').pipe(fs.createWriteStream(`${target}/.prettierrc`));
+
+  // 3. copy JSON file and manipulate it
+  const defaultJSON = JSON.parse(fs.readFileSync('./assets/package.json'));
+  const dirJSON = JSON.parse(fs.readFileSync(`${src}/package.json`));
+  const rootJSON = JSON.parse(fs.readFileSync(`./package.json`));
+  const targetJSON = {
+    ...defaultJSON,
+    ...dirJSON,
+    version: rootJSON.version,
+  };
+  fs.writeFileSync(`${target}/package.json`, JSON.stringify(targetJSON, null, 2), 'utf8');
+
+  // 4. copy HTML file
+  const html = fs.readFileSync('./assets/index.html', 'utf8');
+  const $ = cheerio.load(html, {
+    withDomLvl1: true,
+    normalizeWhitespace: false,
+    xmlMode: true,
+  });
+  $('title').text(targetJSON.description);
+  const text = $.html().replace('<script src="./index.js"> </script>', '<script src="./index.js"></script>');
+  fs.writeFileSync(`${target}/index.html`, text, 'utf8');
+
+  // 5. copy js file
+  const jsFiles = getJSFiles(src);
+  jsFiles.forEach(file => {
+    fs.createReadStream(`${src}/${file}`).pipe(fs.createWriteStream(`${target}/${file}`));
+  });
+
+  // 6. copy sandbox config
+  copyFile('./assets/sandbox.config.json', `${target}/sandbox.config.json`);
+}
+
+const buildDirs = async (dirs) => {
+  mkdirp('./dist');
+  for (let i = 0; i < dirs.length; i++) {
+    console.log(dirs[i]);
+    await buildDir(dirs[i]);
+  }
+}
+
+const prepareBuild = () => new Promise((resolve, reject) => {
+  fs.readdir(src, async (err, dirs) => {
+    try {
+      await buildDirs(dirs);
+    } catch(err) {
+      console.error(err);
+      return reject(err);
+    }
+    console.log('good');
+    return resolve();
+  });
+});
+
+(async () => {
+  await prepareBuild();
+  ghpages.publish('dist', {
+    branch: 'dist',
+  });
+})();
+
