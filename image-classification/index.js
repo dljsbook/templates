083633baf945c935c9ecbbf897b81a49a67c4441
Kevin Scott <kevin@hitheory.com>
2019-02-03 13:54:32 -0500

    Updates

--- image-classification/index.js
+++ image-classification/index.js
@@ -1,8 +1,2 @@
-import * as tf from "@tensorflow/tfjs"
-import * as tfvis from "@tensorflow/tfjs-vis"
-import { ImageNet } from "@dljsbook/data"
-import { MobileNet } from "@dljsbook/models"
-
-tf.loadModel(MobileNet.url).then(mobilenet => {
-  mobilenet.summary()
-})
+const { ImageNet } = dljsData
+const { MobileNet } = dljsModels
