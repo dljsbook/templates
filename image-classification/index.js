123a2433757b9de9b62fd419c18d4bd8c116dd01
Kevin Scott <kevin@hitheory.com>
2019-02-04 13:00:19 -0500

    Updates

--- image-classification/index.js
+++ image-classification/index.js
@@ -2,3 +2,6 @@ const { ImageClassifier } = dljsUI
 
 const imageClassifier = new ImageClassifier(document.getElementById('root'))
 
+imageClassifier.onImages(images => {
+  console.log('images', images)
+})
