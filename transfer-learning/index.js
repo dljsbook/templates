819e1594f59f1d7000d58a4b4e51d1bbf04b1eee
Kevin Scott <kevin@hitheory.com>
2019-02-18 16:47:28 -0500

    Updates

--- transfer-learning/index.js
+++ transfer-learning/index.js
@@ -1,6 +1,4 @@
-import * as tf from "@tensorflow/tfjs"
-import * as tfvis from "@tensorflow/tfjs-vis"
-import { MobileNet } from "@dljsbook/models"
+const { MobileNet } = dljsModels
 
 tf.loadModel(MobileNet.url).then(mobilenet => {
   mobilenet.summary()
